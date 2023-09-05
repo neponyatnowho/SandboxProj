fa8996b67a050dc678f6bed4a9e25834de2cecd5
Potii Roman <romanafksher@gmail.com>
2023-09-05 23:13:33 +0300

    physic car mechanic

---
+++ Assets/Scripts/CarSuspension.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class CarSuspension : MonoBehaviourImproved
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _forceAmount;
+    [SerializeField] private float _suspensionLength;
+
+    [SerializeField] private Transform[] _suspensionTargets;
+    [SerializeField] private Transform _centerOfMass;
+
+    private void Start()
+    {
+        //_rigidbody.centerOfMass = _centerOfMass.position;
+    }
+    //private void FixedUpdate()
+    //{
+
+    //    for (int i = 0; i < _suspensionTargets.Length; i++)
+    //    {
+    //        Vector3 suspensionStart = _suspensionTargets[i].position;
+    //        Vector3 suspensionEnd = _suspensionTargets[i].position - Vector3.up * _suspensionLength;
+
+    //        bool cast = Physics.Linecast(suspensionStart, suspensionEnd);
+
+    //        if (cast)
+    //            _rigidbody.AddForce(_suspensionTargets[i].transform.position + (Vector3.up * _forceAmount), ForceMode.Force);
+
+    //        Color rayColor = cast ? Color.green : Color.red;
+    //        Debug.DrawLine(suspensionStart, suspensionEnd, rayColor);
+    //    }
+    //}
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < _suspensionTargets.Length; i++)
+        {
+            Vector3 suspensionStart = _suspensionTargets[i].position;
+            Vector3 suspensionEnd = _suspensionTargets[i].position - Vector3.up * _suspensionLength;
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(suspensionStart, -Vector3.up, out hit, _suspensionLength))
+            {
+                float distance = Vector3.Distance(suspensionStart, hit.point);
+
+                float modifier = 1f - (distance / _suspensionLength);
+
+                Vector3 forceDirection = _suspensionTargets[i].transform.position + (Vector3.up * _forceAmount * modifier) - _rigidbody.position;
+                //_rigidbody.AddForceAtPosition(forceDirection.normalized * _forceAmount, _suspensionTargets[i].position, ForceMode.Force);
+                _rigidbody.AddForce(forceDirection, ForceMode.Force);
+            }
+
+            Color rayColor = hit.collider ? Color.green : Color.red;
+            Debug.DrawLine(suspensionStart, hit.point, rayColor);
+        }
+    }
+}
